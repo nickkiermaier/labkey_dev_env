66cb384fb142db2590d598f112dff3cefc995966
Nick Kiermaier <nkiermai@tulane.edu>
2021-05-21 09:39:12 -0500

    Update

--- provision/mac/build/1-environment/dependencies.sh
+++ provision/mac/build/1-environment/dependencies.sh
@@ -12,3 +12,6 @@ brew install git zip unzip wget curl dos2unix curl dos2unix
 # add github keygen to prevent first time connection issues(note only do on dev machines)
 ssh-keygen -F github.com || ssh-keyscan github.com >>~/.ssh/known_hosts
 sleep 1
+
+# disable quarantine
+sudo defaults write com.apple.LaunchServices LSQuarantine -bool NO
